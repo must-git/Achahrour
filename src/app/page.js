b915102454d2f50d33400d2a9db5aa5db2034a01
Mustapha <must-git@users.noreply.github.com>
2026-03-07 05:34:39 +0000

    refactor: place vercel analytics comp to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Analytics />
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           src="/arabic_logo.svg"
